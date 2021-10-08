60f92c228fc07d82ea005da179d21a7dd2dfb9ea
Vlada FKZ Laptop <vlada030@gmail.com>
2021-10-08 14:48:31 +0200

    zapocet dark mod sa dodatkom prefers-colors-scheme

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
-import './scss/main.scss';
+import "./scss/main.scss";
 
-const form = document.querySelector('.form')
+const body = document.querySelector("body");
+const form = document.querySelector(".form");
+const button = document.querySelector(".theme");
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-})
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+// lepo objasnjeno
+// https://medium.com/samsung-internet-dev/responsive-design-the-dark-mode-10f8f9ca2c2
+
+button.addEventListener("click", () => {
+    const hasSysDarkClass = document.body.classList.contains("systemDarkPreference");
+
+    const currentSysIsDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+    ).matches;
+
+    const isDark = (hasSysDarkClass && currentSysIsDark) ||
+        document.body.classList.contains("dark");
+
+        console.log({hasSysDarkClass, currentSysIsDark, isDark});
+
+    document.body.classList.remove("systemDarkPreference");
+    document.body.classList.toggle("dark", !isDark);
+});
